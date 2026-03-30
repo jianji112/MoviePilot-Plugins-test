16ee1d4305c6f672b5ac618183e2c558b239756f
jianji112 <jianji112@users.noreply.github.com>
2026-03-30 11:18:51 +0800

    v3.5.22

--- plugins/autosubv3/__init__.py
+++ plugins/autosubv3/__init__.py
@@ -86,7 +86,7 @@ class AutoSubv3(_PluginBase):
     # 主题色
     plugin_color = "#2C4F7E"
     # 插件版本
-    plugin_version = "3.5.21"
+    plugin_version = "3.5.22"
     # 插件作者
     plugin_author = "jianji112"
     # 作者主页
